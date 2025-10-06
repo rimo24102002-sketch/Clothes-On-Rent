b20ff8ddc30942b6b3317f4a8d877212398396e3
rimo24102002 <rimo24102002@gmail.com>
2025-10-06 09:39:28 +0500

    ADMIN work

--- firebase.js
+++ firebase.js
@@ -2,29 +2,15 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
-
-
-
 const firebaseConfig = {
-<<<<<<< HEAD
   apiKey: "AIzaSyAWwZ9f46HBmsyFme-7-rEF9hH-j1nLmxw",
   authDomain: "clothesonrent-bc27e.firebaseapp.com",
   projectId: "clothesonrent-bc27e",
   storageBucket: "clothesonrent-bc27e.firebasestorage.app",
   messagingSenderId: "613854293169",
-  appId: "1:613854293169:web:7bfa1c84ef3be4a1368690",
-  measurementId: "G-CWQ6HPSNSY"
-=======
-  apiKey: "AIzaSyD8C3QMfHaeIx9Y_OpQ2UlDY1Zqq9SNIWQ",
-  authDomain: "clothesonrent-e1ea0.firebaseapp.com",
-  projectId: "clothesonrent-e1ea0",
-  storageBucket: "clothesonrent-e1ea0.firebasestorage.app",
-  messagingSenderId: "835751712580",
-  appId: "1:835751712580:web:f1dcf167faae49b5382911",
-  measurementId: "G-S8ELWZ8EC7"
->>>>>>> 65106e78a021ac908355ed7aefcad582787e962f
+  appId: "1:613854293169:web:3dcef0861ff600b3368690",
+  measurementId: "G-ZL3QSQSDZX"
 };
-
 // Initialize Firebase
 try {
   var  app = initializeApp(firebaseConfig);
